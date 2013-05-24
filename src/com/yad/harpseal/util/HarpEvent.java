eb3a6ab4207ec38c39fc08e1328a888eb99f84f0
NSDB <nsdb@naver.com>
2013-05-24 22:33:06 +0900

    Adding some log in regulate() of HarpEvent

--- src/com/yad/harpseal/util/HarpEvent.java
+++ src/com/yad/harpseal/util/HarpEvent.java
@@ -3,19 +3,25 @@ package com.yad.harpseal.util;
 public class HarpEvent {
 	private int type;
 	private float x,y;
+	private boolean regulated;
 	private boolean processed;
 	
 	public HarpEvent(int type,float x,float y) {
 		this.type=type;
 		this.x=x;
 		this.y=y;
+		this.regulated=false;
 		this.processed=false;
 	}
 	
 	public void regulate(float scaleRate,float transHeight) {
+		if(regulated==true) {
+			HarpLog.error("Event is already regulated");
+		}
 		x/=scaleRate;
 		y/=scaleRate;
 		y-=transHeight;
+		regulated=true;
 	}
 	
 	
