aa6ea046212180369e43bdc371d9a723839a7135
NSDB <nsdb@naver.com>
2013-05-30 00:39:47 +0900

    HarpButton class (Contents are not updated)

--- gen/com/yad/harpseal/R.java
+++ gen/com/yad/harpseal/R.java
@@ -17,8 +17,10 @@ public final class R {
         public static final int activity_main=0x7f030000;
     }
     public static final class raw {
-        public static final int sample_bgm=0x7f040000;
-        public static final int sample_chunk=0x7f040001;
+        public static final int button_no=0x7f040000;
+        public static final int button_ok=0x7f040001;
+        public static final int sample_bgm=0x7f040002;
+        public static final int sample_chunk=0x7f040003;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
