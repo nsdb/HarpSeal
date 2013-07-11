1e2bb8e42ddf1ddacb7c4fc21fccd5f094a4b69e
NSDB <nsdb@naver.com>
2013-07-11 16:23:24 +0900

    Refactoring : Divide GameStage class (Camera)

---
+++ src/com/yad/harpseal/gameobj/stage/GameCamera.java
@@ -0,0 +1,138 @@
+package com.yad.harpseal.gameobj.stage;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+import com.yad.harpseal.constant.Screen;
+import com.yad.harpseal.gameobj.GameObject;
+import com.yad.harpseal.util.Communicable;
+import com.yad.harpseal.util.Func;
+import com.yad.harpseal.util.HarpEvent;
+import com.yad.harpseal.util.HarpLog;
+
+public class GameCamera extends GameObject {
+
+	private float cameraX,cameraY;
+	private float cameraWidth,cameraHeight;
+
+	private int cameraMode;
+	private float targetX,targetY;
+	
+	private final static int CAM_MENUAL=1;
+	private final static int CAM_TARGET=2;
+	private final static float TARGET_MOTION_MIN=1f;
+	private final static float TARGET_MOTION_SPD=0.075f;
+
+	public GameCamera(Communicable con, int width, int height, int x, int y) {
+		super(con);
+		this.cameraX=0;
+		this.cameraY=0;
+		this.cameraWidth=width;
+		this.cameraHeight=height;
+		
+		this.cameraMode=CAM_MENUAL;
+		this.targetX=0;
+		this.targetY=0;
+		setCameraPoint(x,y,false);
+	}
+
+	// In fact, something is wrong.. -_-
+	@Override
+	public void playGame(int ms) {
+		
+		if(cameraMode != CAM_TARGET) return;
+		
+		if(targetX!=cameraX || targetY!=cameraY) {
+			float moveX=(targetX-cameraX)*TARGET_MOTION_SPD*ms/15;
+			float moveY=(targetY-cameraY)*TARGET_MOTION_SPD*ms/15;
+			float minPer=Func.distan(0, 0, moveX, moveY)/TARGET_MOTION_MIN*ms/15;
+			if(minPer<1) {
+				moveX/=minPer;
+				moveY/=minPer;
+			}
+			
+			if(Func.distan(targetX,targetY,cameraX,cameraY)<TARGET_MOTION_MIN*ms/15) {
+				cameraX=targetX;
+				cameraY=targetY;
+			}
+			else {
+				cameraX+=moveX;
+				cameraY+=moveY;
+			}
+		}
+		if(targetX==cameraX && targetY==cameraY) {
+			cameraMode=CAM_MENUAL;
+			HarpLog.info("Camera mode change : "+cameraMode);
+		}
+	}
+
+	@Override
+	public void receiveMotion(HarpEvent ev, int layer) {
+	}
+
+	@Override
+	public void drawScreen(Canvas c, Paint p, int layer) {
+	}
+
+	@Override
+	public void restoreData() {
+	}
+
+	@Override
+	public int send(String msg) {
+		String[] msgs=msg.split("/");
+
+		if(msgs[0].equals("playerMoved")) {
+			int x=Integer.parseInt(msgs[1]);
+			int y=Integer.parseInt(msgs[2]);
+			if(autoTargetingCheck(x,y)==true)
+				setCameraPoint(x,y,true);
+			return 1;
+		}
+		else if(msgs[0].equals("scroll")) {
+			cameraMode=CAM_MENUAL;
+			cameraX+=Float.parseFloat(msgs[1]);
+			cameraY+=Float.parseFloat(msgs[2]);
+			regulateCamera();
+			return 1;
+		}
+		else return 0;
+	}
+
+	@Override
+	public Object get(String name) {
+		if(name.equals("cameraX"))
+			return cameraX;
+		else if(name.equals("cameraY"))
+			return cameraY;
+		else return null;
+	}
+	
+	// private
+
+	private void setCameraPoint(float x, float y, boolean animate) {
+		if(animate) {
+			cameraMode=CAM_TARGET;
+			targetX=x-Screen.SCREEN_X/2;
+			targetY=y-Screen.SCREEN_Y/2;
+			targetX=Func.limit(targetX, 0, cameraWidth-Screen.SCREEN_X);
+			targetY=Func.limit(targetY, 0,  cameraHeight-Screen.SCREEN_Y);
+			HarpLog.info("Camera mode change : "+cameraMode);
+		} else {
+			cameraX=x-Screen.SCREEN_X/2;
+			cameraY=y-Screen.SCREEN_Y/2;
+			regulateCamera();
+		}
+	}
+	
+	private void regulateCamera() { 
+		cameraX=Func.limit(cameraX, 0, cameraWidth-Screen.SCREEN_X);
+		cameraY=Func.limit(cameraY, 0,  cameraHeight-Screen.SCREEN_Y);
+	}
+	
+	private boolean autoTargetingCheck(float x, float y) {
+		return (x < cameraX+Screen.TILE_LENGTH || x > cameraX+Screen.SCREEN_X-Screen.TILE_LENGTH ||
+				y < cameraY+Screen.TILE_LENGTH || y > cameraY+Screen.SCREEN_Y-Screen.TILE_LENGTH);			
+	}
+	
+}
--- src/com/yad/harpseal/gameobj/stage/GameStage.java
+++ src/com/yad/harpseal/gameobj/stage/GameStage.java
@@ -4,33 +4,25 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 
 import com.yad.harpseal.constant.Layer;
-import com.yad.harpseal.constant.Screen;
 import com.yad.harpseal.gameobj.GameObject;
 import com.yad.harpseal.gameobj.window.Joystick;
 import com.yad.harpseal.gameobj.window.ScoreCounter;
 import com.yad.harpseal.util.Communicable;
-import com.yad.harpseal.util.Func;
 import com.yad.harpseal.util.HarpEvent;
 import com.yad.harpseal.util.HarpLog;
 
 public class GameStage extends GameObject {
 	
-	// map
+	// stage status
 	private int stageGroup;
 	private int stageNumber;
+	private int stepCount;
+	private int fishCount;
+	private final static int FISH_MAX=3;
+
+	// game objects
 	private GameMap map;
-
-	// camera
-	private float cameraX,cameraY;
-	private float cameraWidth,cameraHeight;
-	private int cameraMode;
-	private float targetX,targetY;
-	private final static int CAM_MENUAL=1;
-	private final static int CAM_TARGET=2;
-	private final static float TARGET_MOTION_MIN=1f;
-	private final static float TARGET_MOTION_SPD=0.075f;
-
-	// user interfaces
+	private GameCamera camera;
 	private Joystick stick;
 	private ScoreCounter counter;
 	
@@ -40,45 +32,27 @@ public class GameStage extends GameObject {
 	private final static int ACT_STARTING_FADEOUT=1;
 	private final static int ACT_PLAYING=2;
 	
-	// score data
-	private int stepCount;
-	private int fishCount;
-	private final static int FISH_MAX=3;
-	
-	
-	//// public method
-	
-	
+
 	public GameStage(Communicable con, int stageGroup, int stageNumber) {
 		super(con);
 		
-		// map
+		// stage status
 		this.stageGroup=stageGroup;
 		this.stageNumber=stageNumber;
-		map=new GameMap(this,stageGroup,stageNumber);
+		this.stepCount=0;
+		this.fishCount=0;
 		////
 		
-		// user interface
+		// game objects
+		map=new GameMap(this,stageGroup,stageNumber);
+		camera=new GameCamera( this, (Integer)map.get("width"), (Integer)map.get("height"),
+				(Integer)map.get("playerX"), (Integer)map.get("playerY") );
 		stick=new Joystick(this);
 		counter=new ScoreCounter(this);
 		
-		// camera point init
-		cameraX=0;
-		cameraY=0;
-		cameraWidth=(Integer)map.get("width");
-		cameraHeight=(Integer)map.get("height");
-		cameraMode=CAM_MENUAL;
-		targetX=0;
-		targetY=0;
-		setCameraPoint( (Integer)map.get("playerX"), (Integer)map.get("playerY") );
-		
-		// stage init
+		// stage action
 		actionName=ACT_STARTING_FADEOUT;
 		actionTime=0;
-		
-		// score data init
-		stepCount=0;
-		fishCount=0;
 	}
 
 	@Override
@@ -86,11 +60,9 @@ public class GameStage extends GameObject {
 
 		// objects
 		map.playGame(ms);
+		camera.playGame(ms);
 		stick.playGame(ms);
 		counter.playGame(ms);
-		
-		// camera
-		animateCamera(ms);
 		
 		// controller
 		actionTime+=ms;
@@ -104,11 +76,13 @@ public class GameStage extends GameObject {
 	public void receiveMotion(HarpEvent ev, int layer) {
 
 		// camera
-		if(layer==Layer.LAYER_FIELD) ev.setCamera(cameraX, cameraY);
+		if(layer==Layer.LAYER_FIELD) ev.setCamera( (Float)camera.get("cameraX"), (Float)camera.get("cameraY") );
 		else if(layer==Layer.LAYER_WINDOW) ev.setCamera(0, 0);
 		
 		// objects
 		map.receiveMotion(ev, layer);
+		if(ev.isProcessed()) return;
+		camera.receiveMotion(ev, layer);
 		if(ev.isProcessed()) return;
 		stick.receiveMotion(ev, layer);
 		if(ev.isProcessed()) return;
@@ -119,11 +93,12 @@ public class GameStage extends GameObject {
 	public void drawScreen(Canvas c, Paint p, int layer) {
 		
 		// camera
-		if(layer==Layer.LAYER_FIELD) c.translate(-cameraX,-cameraY);
-		else if(layer==Layer.LAYER_WINDOW) c.translate(cameraX,cameraY);
+		if(layer==Layer.LAYER_FIELD) c.translate( -(Float)camera.get("cameraX"), -(Float)camera.get("cameraY") );
+		else if(layer==Layer.LAYER_WINDOW) c.translate( (Float)camera.get("cameraX"), (Float)camera.get("cameraY") );
 		
 		// objects
 		map.drawScreen(c, p, layer);
+		camera.drawScreen(c, p, layer);
 		stick.drawScreen(c, p, layer);
 		counter.drawScreen(c, p, layer);
 		
@@ -148,9 +123,8 @@ public class GameStage extends GameObject {
 		String[] msgs=msg.split("/");
 
 		if(msgs[0].equals("stickAction")) {
-			map.send("movePlayer/"+msgs[1]);			
-			if(autoCameraRegulateCheck( (Integer)map.get("playerX"), (Integer)map.get("playerY") ))
-				setCameraPoint( (Integer)map.get("playerX"), (Integer)map.get("playerY") );
+			map.send("movePlayer/"+msgs[1]);
+			camera.send("playerMoved/"+(Integer)map.get("playerX")+"/"+(Integer)map.get("playerY"));
 			return 1;
 		}
 		else if(msgs[0].equals("playerStepped")) {
@@ -168,12 +142,7 @@ public class GameStage extends GameObject {
 			return 1;
 		}
 		else if(msgs[0].equals("scroll")) {
-			if(cameraMode==CAM_MENUAL) {
-				cameraX+=Float.parseFloat(msgs[1]);
-				cameraY+=Float.parseFloat(msgs[2]);
-				regulateCamera();
-				return 1;
-			} else return 0;
+			return camera.send(msg);
 		}
 		else return con.send(msg);
 	}
@@ -188,70 +157,6 @@ public class GameStage extends GameObject {
 			return FISH_MAX;
 		else
 			return con.get(name);
-	}
-	
-	
-	//// private method (game play)
-	
-	
-	
-	private void setCameraPoint(float x,float y) {
-		cameraMode=CAM_TARGET;
-		targetX=x-Screen.SCREEN_X/2;
-		targetY=y-Screen.SCREEN_Y/2;
-		targetX=Func.limit(targetX, 0, cameraWidth-Screen.SCREEN_X);
-		targetY=Func.limit(targetY, 0,  cameraHeight-Screen.SCREEN_Y);
-		HarpLog.info("Camera mode change : "+cameraMode);
-		regulateCamera();
-	}
-	
-	private void regulateCamera() { 
-		cameraX=Func.limit(cameraX, 0, cameraWidth-Screen.SCREEN_X);
-		cameraY=Func.limit(cameraY, 0,  cameraHeight-Screen.SCREEN_Y);
-	}
-	
-	// In fact, something is wrong.. -_-
-	private void animateCamera(int ms) {
-		
-		switch(cameraMode) {
-		
-		case CAM_MENUAL:
-			regulateCamera();
-			break;
-		
-		case CAM_TARGET:
-			if(targetX!=cameraX || targetY!=cameraY) {
-				float moveX=(targetX-cameraX)*TARGET_MOTION_SPD*ms/15;
-				float moveY=(targetY-cameraY)*TARGET_MOTION_SPD*ms/15;
-				float minPer=Func.distan(0, 0, moveX, moveY)/TARGET_MOTION_MIN*ms/15;
-
-				if(minPer<1) {
-					moveX/=minPer;
-					moveY/=minPer;
-				}
-				
-				if(Func.distan(targetX,targetY,cameraX,cameraY)<TARGET_MOTION_MIN*ms/15) {
-					cameraX=targetX;
-					cameraY=targetY;
-				}
-				else {
-					cameraX+=moveX;
-					cameraY+=moveY;
-				}
-			}
-			if(targetX==cameraX && targetY==cameraY) {
-				cameraMode=CAM_MENUAL;
-				HarpLog.info("Camera mode change : "+cameraMode);
-			}
-			break;
-		
-		}		
-	}
-	
-	private boolean autoCameraRegulateCheck(float x, float y) {
-		return (x < cameraX+Screen.TILE_LENGTH || x > cameraX+Screen.SCREEN_X-Screen.TILE_LENGTH ||
-				y < cameraY+Screen.TILE_LENGTH || y > cameraY+Screen.SCREEN_Y-Screen.TILE_LENGTH);
-			
 	}
 	
 	
